81f6b5900019a7e67b577af37e425a4712bed5a6
elianamuthui-coder <elianamuthui@gmail.com>
2026-06-04 11:53:16 +0300

    lab js 2.7

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let age = 20;
+var course = "BBIT";
+const university = "Strathmore";
+// Redeclare.
+// let age = 40;
+var course = "BBIT";
+// const university = "Strathmore";
+age = 40;
+course = "ICS";
+if(true){
+    let time = "11:30";
+    var day = "4th";
+    const year = 2026;
+
+}
+
+console.log(day);
